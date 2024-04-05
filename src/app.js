580ba74dcf4841dcc7bba8a7f0b3044f686e2a9b
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-04-05 10:12:14 +0530

    update the filter button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
+import React from "react";
 import reactDom from "react-dom/client";
-
 import Body from "./components/Body"
 import Header from "./components/Header"
 import {Header} from "./components/Header"
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,19 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/datas";
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants]=useState(resList)
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
-                    console.log('first')
-                    resList = resList.filter(res => res.info.avgRating > 4)
+                
+                    const filteredRestaurants = listOfRestaurants.filter(res => res.info.avgRating > 4);
+                    setListOfRestaurants(filteredRestaurants);
                 }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
-                {resList.map((res) =><RestaurantCard key={res.info.id} resData={res}/>
+                {listOfRestaurants.map((res) =><RestaurantCard key={res.info.id} resData={res}/>
                 )}
             </div>
         </div>
